a1922d87cf6677eaa3fea6536cb3d2577f41f74e
Mo'ath Bdour <moathbdour@users.noreply.github.com>
2023-01-30 16:10:25 +0300

    The Project

---
+++ Dingo/Project7/WebForm9.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm9.aspx.cs" Inherits="Project7.WebForm9" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server">
+            <Columns>
+                <asp:HyperLinkField DataNavigateUrlFields="ids" HeaderText="Delete" Text="Delete" DataNavigateUrlFormatString="WebForm9.aspx?id={0}" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Dingo/Project7/WebForm9.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project7
+{
+    
+    public partial class WebForm9 : System.Web.UI.Page
+    {
+        project7Entities data = new project7Entities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"]!=null)
+            {
+                int idss = Convert.ToInt32(Request.QueryString["id"]);
+                var deletes = data.buffet_menu_items.Find(idss);
+                data.buffet_menu_items.Remove(deletes);
+                data.SaveChanges();
+               // Response.Redirect("WebForm9.aspx");
+            }
+
+            var obj = data.buffet_menu_items.ToList();
+            GridView1.DataSource= obj;
+            GridView1.DataBind();
+        }
+    }
+}
